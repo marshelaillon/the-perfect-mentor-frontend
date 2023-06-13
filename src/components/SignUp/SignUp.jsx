bfb60eaa5abb06871f4d4850be4ec1fbedfdf5e2
marshel <marshel.aillon@gmail.com>
2023-06-13 17:15:49 -0400

    basic validations and form with values ready to send to API

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,25 +1,14 @@
-// import { useFormik } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import title from '../../assets/title.png';
 import doodle1 from '../../assets/doodle-1.png';
 import doodle2 from '../../assets/doodle-2.png';
 import emailIcon from '../../assets/email-icon.png';
 import passwordIcon from '../../assets/password-icon.png';
 import './SignUp.css';
-//import { useGetRolesQuery } from '../../services/thePerfectMentorApi';
+import { useGetRolesQuery } from '../../services/thePerfectMentorApi';
 
 export default function SignUp() {
-  //const { data: roles, isLoading } = useGetRolesQuery();
-  // const formik = useFormik({
-  //   initialValues: {
-  //     email: '',
-  //     password: '',
-  //     confirmPassword: '',
-  //     roleId: '',
-  //   },
-  //   onSubmit(values) {
-  //     console.log(values);
-  //   },
-  // });
+  const { data: roles, isLoading } = useGetRolesQuery();
 
   return (
     <div className="sign-form-container gap-1">
@@ -36,66 +25,125 @@ export default function SignUp() {
           className="absolute doodle-sign-up-2"
         />
       </div>
-      <form className="flex flex-col gap-3">
-        <div className="flex flex-col border-2 border-primary-dark p-8 sign-up-borders gap-2">
-          <h2 className="font-bold capitalize text-2xl text-primary-dark">
-            sign up
-          </h2>
 
-          <div className="relative">
-            <span className="absolute top-2 left-2 bg-primary-white p-3 rounded-full">
-              <img src={emailIcon} alt="email icon" />
-            </span>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+          confirmPassword: '',
+          roleId: '',
+        }}
+        onSubmit={(values, { resetForm }) => {
+          console.log(values);
+          resetForm();
+        }}
+        validate={values => {
+          const errors = {};
+          if (values.password !== values.confirmPassword) {
+            errors.passDontMatchMsg = 'Passwords do not match';
+          }
+          if (!values.password) {
+            errors.passDontMatchMsg = 'Enter a password';
+          }
+          if (!values.email) {
+            errors.requiredEmail = 'Please, enter an email';
+          }
+          if (!values.roleId) {
+            errors.requiredRole = 'Please, select a role';
+          }
+          return errors;
+        }}
+      >
+        {({ values, touched, errors, handleChange, handleBlur }) => (
+          <Form className="flex flex-col gap-3">
+            <div className="flex flex-col border-2 border-primary-dark p-8 sign-up-borders gap-2">
+              <h2 className="font-bold capitalize text-2xl text-primary-dark">
+                sign up
+              </h2>
 
-            <input
-              className="py-3 pl-12 pr-5 outline-none bg-transparent border-2 border-primary-dark placeholder-primary-gray rounded-full text-primary-gray"
-              type="email"
-              placeholder="email"
-            />
-          </div>
+              <div className="relative">
+                <span className="absolute top-2 left-2 bg-primary-white p-3 rounded-full">
+                  <img src={emailIcon} alt="email icon" />
+                </span>
 
-          <div className="relative">
-            <span className="absolute top-2 left-2 bg-primary-white p-3 rounded-full">
-              <img src={passwordIcon} alt="password icon" />
-            </span>
+                <Field
+                  className="py-3 pl-12 pr-5 outline-none bg-transparent border-2 border-primary-dark placeholder-primary-gray rounded-full text-primary-gray"
+                  type="email"
+                  placeholder="email"
+                  name="email"
+                />
+                {touched.email && errors.requiredEmail && (
+                  <p className="py-2 px-5 text-red-500 text-xs">
+                    {errors.requiredEmail}
+                  </p>
+                )}
+              </div>
 
-            <input
-              className="py-3 pl-12 pr-5 outline-none bg-transparent border-2 border-primary-dark placeholder-primary-gray rounded-full text-primary-gray"
-              type="password"
-              placeholder="password"
-            />
-          </div>
+              <div className="relative">
+                <span className="absolute top-2 left-2 bg-primary-white p-3 rounded-full">
+                  <img src={passwordIcon} alt="password icon" />
+                </span>
 
-          <div className="relative">
-            <span className="absolute top-2 left-2 bg-primary-white p-3 rounded-full">
-              <img src={passwordIcon} alt="password icon" />
-            </span>
+                <Field
+                  className="py-3 pl-12 pr-5 outline-none bg-transparent border-2 border-primary-dark placeholder-primary-gray rounded-full text-primary-gray"
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                />
+              </div>
 
-            <input
-              className="py-3 pl-12 pr-5 outline-none bg-transparent border-2 border-primary-dark placeholder-primary-gray rounded-full text-primary-gray"
-              type="password"
-              placeholder="confirm password"
-            />
-          </div>
+              <div className="relative">
+                <span className="absolute top-2 left-2 bg-primary-white p-3 rounded-full">
+                  <img src={passwordIcon} alt="password icon" />
+                </span>
 
-          <select
-            name="roles"
-            id="roles"
-            className="bg-primary-yellow py-3 px-8 outline-none bg-transparent border-2 border-primary-dark placeholder-primary-gray rounded-full text-primary-gray"
-          >
-            {/* <option value="null">-- Select a role --</option>
-            {!isLoading &&
-              roles.map(({ id, name }) => (
-                <option key={id} value={id}>
-                  {name[0].toUpperCase() + name.slice(1)}
-                </option>
-              ))} */}
-          </select>
-        </div>
-        <button className="align-center bg-primary-dark text-primary-white py-3 rounded-full capitalize">
-          sign up
-        </button>
-      </form>
+                <Field
+                  className="py-3 pl-12 pr-5 outline-none bg-transparent border-2 border-primary-dark placeholder-primary-gray rounded-full text-primary-gray"
+                  type="password"
+                  placeholder="confirm password"
+                  name="confirmPassword"
+                />
+                {touched.confirmPassword && errors.passDontMatchMsg && (
+                  <p className="py-2 px-5 text-red-500 text-xs">
+                    {errors.passDontMatchMsg}
+                  </p>
+                )}
+              </div>
+
+              <Field
+                as="select"
+                name="roleId"
+                id="roleId"
+                className="bg-primary-yellow py-3 px-8 outline-none bg-transparent border-2 border-primary-dark placeholder-primary-gray rounded-full text-primary-gray"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.roleId}
+              >
+                <option value="">-- Select a role --</option>
+                {!isLoading &&
+                  roles.map(({ _id, name }) => (
+                    <option key={_id} value={_id}>
+                      {name[0].toUpperCase() + name.slice(1)}
+                    </option>
+                  ))}
+              </Field>
+
+              {touched.roleId && errors.requiredRole && (
+                <p className="py-2 px-5 text-red-500 text-xs">
+                  {errors.requiredRole}
+                </p>
+              )}
+            </div>
+
+            <button
+              type="submit"
+              className="align-center bg-primary-dark text-primary-white py-3 rounded-full capitalize"
+            >
+              sign up
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
