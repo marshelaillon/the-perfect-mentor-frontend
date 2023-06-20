ea3a3b164211354dc96449a6f637c415cf334560
marshel <marshel.aillon@gmail.com>
2023-06-20 12:26:48 -0400

    generic password in profile

--- src/components/MyProfile/MyProfile.jsx
+++ src/components/MyProfile/MyProfile.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import { NavigationBar } from '../index';
+import { useDispatch } from 'react-redux';
+import { useGetMeQuery } from '../../services/thePerfectMentorApi';
+import { setUser } from '../../app/features/userSlice';
 import editIcon from '../../assets/editIcon.svg';
 import profilePic from '../../assets/profile.svg';
 import './MyProfile.css';
@@ -8,12 +10,20 @@ import '../../styles/loader.css';
 
 export default function MyProfile() {
   const [editing, setIsEditing] = useState(false);
-
-  const user = useSelector(state => state.user);
+  const dispatch = useDispatch();
+  const { data: user, isLoading, isSuccess } = useGetMeQuery();
 
   const activateEdit = () => {
     setIsEditing(prevState => !prevState);
   };
+
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(setUser(user));
+    }
+  }, [user, dispatch, isSuccess]);
+
+  if (isLoading) return <p className="loader"></p>;
 
   return (
     <div className="profile-container full-height">
@@ -68,7 +78,7 @@ export default function MyProfile() {
               id="password"
               type="password"
               disabled={!editing}
-              value={user?.password}
+              value={'hereyournewpassword'}
             />
 
             <label htmlFor="age" className="text-sm text-primary-gray">
@@ -97,64 +107,6 @@ export default function MyProfile() {
       </div>
 
       <NavigationBar />
-
-      {/* <div className="bg-primary-dark flex justify-around pt-5 pb-8 rounded-t-3xl nav-container relative">
-        <button className="cursor-pointer nav-button-icon">
-          <img src={addIcon} alt="social icon" />
-        </button>
-
-        <button className="cursor-pointer nav-button-icon">
-          <img src={statsIcon} alt="stats icon" />
-        </button>
-
-        <button className="cursor-pointer nav-button-icon">
-          <img src={notesIcon} alt="notes icon" />
-        </button>
-
-        <button
-          className="cursor-pointer dropdown"
-          onClick={() => setDropdownOpen(!dropdownOpen)}
-        >
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle
-              cx="11.9999"
-              cy="6.8278"
-              r="4.66468"
-              stroke="#BFD732"
-              strokeOpacity={`${pathname === '/profile' ? 1 : 0.3}`}
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M20.846 21.7749V19.8766C20.846 17.6674 19.0552 15.8766 16.846 15.8766H7.15381C4.94467 15.8766 3.15381 17.6674 3.15381 19.8766V21.7749H12.6034"
-              stroke="#BFD732"
-              strokeOpacity={`${pathname === '/profile' ? 1 : 0.3}`}
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </button>
-
-        {dropdownOpen && (
-          <nav className="absolute bg-primary-dark  right my-profile-nav rounded-t-md">
-            <ul className="flex flex-col">
-              <li className="px-3 py-2">
-                <button onClick={clearData} className="text-primary-white">
-                  Log out
-                </button>
-              </li>
-            </ul>
-          </nav>
-        )}
-      </div> */}
     </div>
   );
 }
